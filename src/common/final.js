f8682095e1f20dc9828ac24950dc5111900476d0
ShirakawaTyu <1400070301@qq.com>
2024-02-25 18:16:28 +0800

    改日期

--- src/common/final.js
+++ src/common/final.js
@@ -1,7 +1,7 @@
 // 记得用斜杠，否则IOS会不正常
-export const START_TIME = new Date("2023/8/28").getTime();
-export const END_TIME = new Date("2024/1/15").getTime();
+export const START_TIME = new Date("2024/2/26").getTime();
+export const END_TIME = new Date("2024/7/14").getTime();
 export const TOTAL_WEEK = 19;
 export const BASE_URL = "";
 export const VERSION = 0.39;
-export const WEB_VERSION = 0.40;
+export const WEB_VERSION = 0.41;
