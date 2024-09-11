05e400940b279afd60166a7225161b2b107e41ca
ShirakawaTyu <40626158+flben233@users.noreply.github.com>
2024-09-11 11:23:56 +0800

    other: 修改APP版本号

--- src/common/final.js
+++ src/common/final.js
@@ -3,6 +3,6 @@ export const START_TIME = new Date("2024/9/2").getTime();
 export const END_TIME = new Date("2025/1/13").getTime();
 export const TOTAL_WEEK = 19;
 export const BASE_URL = "";
-export const VERSION = 0.42;
+export const VERSION = 0.41;
 export const WEB_VERSION = "1.0"
 export const NOTICE = "(有点重要的) 姑且做了点安全升级，建议更新一下APP防止用不了，反馈QQ群：706437676 "
