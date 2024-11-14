a1665dd690ee40fd81f527fb9eaf2ee7478099ea
Aliothmoon <aliothmoon@outlook.com>
2024-11-14 13:19:37 +0800

    feat: 新增一卡通管理

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,15 +1,22 @@
 <template>
-    <van-grid-item :text="text" @click="$router.push('/plugin/' + encodeURIComponent(src) + '/' + title)">
-        <template #icon>
-            <van-image :src="icon" style="width: var(--van-grid-item-icon-size);"/>
-        </template>
-    </van-grid-item>
+  <van-grid-item :text="text" @click="click">
+    <template #icon>
+      <van-image :src="icon" style="width: var(--van-grid-item-icon-size);"/>
+    </template>
+  </van-grid-item>
 </template>
 
 <script>
 export default {
-    name: "AppButton",
-    props: ["text", "title", "src", "icon"]
+  name: "AppButton",
+  props: ["text", "title", "src", "icon", 'placeholder'],
+  methods: {
+    click() {
+      if (!this.placeholder) {
+        this.$router.push('/plugin/' + encodeURIComponent(this.src) + '/' + this.title)
+      }
+    }
+  }
 }
 </script>
 
